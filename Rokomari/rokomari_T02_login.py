17c4e8c065a342c29447f877055718eefc7fa883
Sadia Tamim Dip <sadia15-8638@diu.edu.bd>
2023-06-05 23:40:36 +0600

    first commit

---
+++ Rokomari/rokomari_T02_login.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+import time
+
+driver = webdriver.Chrome(executable_path="Webdriver/chromedriver.exe")
+
+driver.maximize_window()
+
+driver.get("https://www.rokomari.com/login")
+print(driver.title)
+driver.find_element(By.XPATH,"/html/body/header/div/div/div/div/div[3]/div/div[2]/a").click()
+driver.find_element(By.XPATH,"//*[@id='js--login-form']/div[2]/div/div/input[2]").send_keys("01xxxxxxxxx")
+driver.find_element(By.ID,"js--btn-next").click()
+time.sleep(1)
+driver.find_element(By.NAME,"j_password").send_keys("1234wea")
+driver.find_element(By.XPATH,"//*[@id='login-form']/button").click()
+time.sleep(3)
