a9bf60e7922f7a40fe1d6e107f0ce71adbf09693
Sadia Tamim Dip <sadia15-8638@diu.edu.bd>
2023-06-08 03:34:42 +0600

    first commit

---
+++ Chaldal/chaldal_T02_login.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+import time
+
+driver = webdriver.Chrome(executable_path="Webdriver/chromedriver.exe")
+
+driver.maximize_window()
+
+driver.get("https://chaldal.com/")
+print(driver.title)
+
+driver.find_element(By.XPATH,"//*[@id='page']/div/div[3]/div/div[1]/div[1]/div[4]/button").click()
+driver.find_element(By.XPATH,"//*[@id='page']/div/div[1]/div/div/div/div[2]/div/form/div[1]/div/div[1]/input").send_keys("01xxxxxxxxx")
+time.sleep(1)
+driver.find_element(By.XPATH,"//*[@id='page']/div/div[1]/div/div/div/div[2]/div/form/div[3]/button").click()
+time.sleep(3)
